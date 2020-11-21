8c42169d94a9bfe8f4aa3641987d3667c5dd39b8
UnseenSevet <59371356+UnseenSevet@users.noreply.github.com>
2020-11-21 11:08:16 -0500

    Update Rainbow.cs

--- Rainbow.cs
+++ Rainbow.cs
@@ -22,10 +22,10 @@ namespace RainbowPluginThingy {
         }
         
         static string[] colors = { "9400D3", "4B0082", "0000FF", "00FF00", "FFFF00", "FF7F00", "FF0000" };
+        System.Threading.Thread.Sleep(700);
         static int index;
         static void RainbowCallback(SchedulerTask task) {
-        	index = (index + 1) % colors.Length;
-            System.Threading.Thread.Sleep(700);
+        	index = (index + 1) % colors.Length; 
         	ColorDesc desc = Colors.ParseHex(colors[index]);
             desc.Code = 'r';
         	Player[] players = PlayerInfo.Online.Items;
