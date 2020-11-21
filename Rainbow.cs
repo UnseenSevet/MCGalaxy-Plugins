40950766588f19ac95281fd0a1528863022574c8
UnseenSevet <59371356+UnseenSevet@users.noreply.github.com>
2020-11-21 11:24:07 -0500

    Update Rainbow.cs

--- Rainbow.cs
+++ Rainbow.cs
@@ -24,7 +24,7 @@ namespace RainbowPluginThingy {
         static string[] colors = { "9400D3", "4B0082", "0000FF", "00FF00", "FFFF00", "FF7F00", "FF0000" };
         static int index;
         static void RainbowCallback(SchedulerTask task) {
-        System.Threading.Thread.Sleep(150);    
+        System.Threading.Thread.Sleep(75);    
         	index = (index + 1) % colors.Length; 
         	ColorDesc desc = Colors.ParseHex(colors[index]);
             desc.Code = 'r';
